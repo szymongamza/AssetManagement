f9d6a9abe5da25b45a1fb6d3680bc82216e734cd
Szymon Gamża <gamza.szymon@gmail.com>
2022-10-24 17:12:50 +0200

    Working with Room BuildingId check

--- Localisation.API/Controllers/RoomsController.cs
+++ Localisation.API/Controllers/RoomsController.cs
@@ -4,6 +4,7 @@ using Localisation.API.Dtos;
 using Localisation.API.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace Localisation.API.Controllers
 {
@@ -11,39 +12,46 @@ namespace Localisation.API.Controllers
     [ApiController]
     public class RoomsController : ControllerBase
     {
-        private readonly IRoomRepo _repository;
+        private readonly IRoomRepo _roomRepo;
+        private readonly IBuildingRepo _buildingRepo;
         private readonly IMapper _mapper;
 
-        public RoomsController(IRoomRepo repository, IMapper mapper)
+        public RoomsController(IRoomRepo roomRepo,IBuildingRepo buildingRepo, IMapper mapper)
         {
-            _repository = repository;
+            _roomRepo = roomRepo;
+            _buildingRepo = buildingRepo;
             _mapper = mapper;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<RoomReadDto>>> GetBuildings()
+        public async Task<ActionResult<IEnumerable<RoomReadDto>>> GetRooms()
         {
-            var rooms = await _repository.GetAllRooms();
+            var rooms = await _roomRepo.GetAllRooms();
             return Ok(_mapper.Map<IEnumerable<RoomReadDto>>(rooms));
         }
 
         [HttpGet("{id}", Name = "GetRoomById")]
         public async Task<ActionResult<IEnumerable<RoomReadDto>>> GetRoomById(int id)
         {
-            var room = await _repository.GetRoomById(id);
+            var room = await _roomRepo.GetRoomById(id);
             if (room != null)
                 return Ok(_mapper.Map<RoomReadDto>(room));
             return NotFound();
         }
 
         [HttpPost]
-        public async Task<ActionResult> CreateBuilding(RoomCreateDto room)
+        public async Task<ActionResult> CreateRoom(RoomCreateDto room)
         {
+            var building = await _buildingRepo.GetBuildingById(room.BuildingId);
+            if (building == null)
+            {
+                return Content(); //TODO
+            }
             var roomModel = _mapper.Map<Room>(room);
-            await _repository.CreateRoom(roomModel);
+            await _roomRepo.CreateRoom(roomModel);
 
-            var buildingReadDto = _mapper.Map<BuildingReadDto>(roomModel);
-            return CreatedAtRoute(nameof(GetRoomById), new { Id = buildingReadDto.Id }, buildingReadDto);
+            var roomReadDto = _mapper.Map<RoomReadDto>(roomModel);
+            return CreatedAtRoute(nameof(GetRoomById), new { Id = roomReadDto.Id }, roomReadDto);
         }
     }
 }

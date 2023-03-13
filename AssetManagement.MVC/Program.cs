5c9fc79f02bff788e770f1578263a3d216f0debe
Szymon Gamża <gamza.szymon@gmail.com>
2023-03-13 23:04:02 +0100

    Changed homepage

--- AssetManagement.MVC/Program.cs
+++ AssetManagement.MVC/Program.cs
@@ -28,7 +28,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
 
 
 app.Run();

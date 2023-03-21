23851b679408c9f3746dc924a3dc8cf1588dfd72
Szymon Gamża <gamza.szymon@gmail.com>
2023-03-21 22:32:09 +0100

    Updated with proper relationships

--- AssetManagement.Domain/Entities/Asset.cs
+++ AssetManagement.Domain/Entities/Asset.cs
@@ -5,15 +5,16 @@ namespace AssetManagement.Domain.Entities;
 public class Asset : BaseAuditableEntity
 {
     public string Name { get; set; } = null!;
+    public int? ManufacturerId { get; set; }
     public Manufacturer? Manufacturer { get; set; }
     public string? ManufacturerSerialNumber { get; set; }
     public DateTime? DateTimeOfBuy { get; set; }
     public DateTime? DateTimeOfNextMaintenance { get; set; }
     public DateTime? DateTimeOfEndOfGuarantee { get; set; }
     public string? AdditionalDescription { get; set; }
-    public Guid QRCode { get; set; } = Guid.NewGuid();
+    public Guid QrCode { get; set; } = Guid.NewGuid();
     public int RoomId { get; set; }
     public Room Room { get; set; } = null!;
-    public ICollection<Maintenance>? Maintenances { get; set; } = new List<Maintenance>();
+    public ICollection<Maintenance> Maintenances { get; set; } = new List<Maintenance>();
 
 }

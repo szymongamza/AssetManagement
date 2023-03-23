48edf7e57e6634eeaf5efed9cd96a5664d1dc5ef
Szymon Gamża <gamza.szymon@gmail.com>
2023-03-24 00:34:52 +0100

    Migrations

--- AssetManagement.Infrastructure/Migrations/20230323182554_DeletedStreetNumber.Designer.cs
+++ AssetManagement.Infrastructure/Migrations/20230323182554_DeletedStreetNumber.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AssetManagement.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AssetManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(AssetManagementContext))]
-    partial class AssetManagementContextModelSnapshot : ModelSnapshot
+    [Migration("20230323182554_DeletedStreetNumber")]
+    partial class DeletedStreetNumber
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.3");
---
+++ AssetManagement.Infrastructure/Migrations/20230323182554_DeletedStreetNumber.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AssetManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeletedStreetNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StreetNumber",
+                table: "Buildings");
+
+            migrationBuilder.RenameColumn(
+                name: "QRCode",
+                table: "Assets",
+                newName: "QrCode");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QrCode",
+                table: "Assets",
+                newName: "QRCode");
+
+            migrationBuilder.AddColumn<int>(
+                name: "StreetNumber",
+                table: "Buildings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}

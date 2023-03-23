ef011790011f73b81330c70a4bb527ad9eb08b15
Szymon Gamża <gamza.szymon@gmail.com>
2023-03-24 00:36:48 +0100

    Added BuildingViewModel with MappingProfile

---
+++ AssetManagement.MVC/Helpers/MvcMappingProfiles.cs
@@ -0,0 +1,24 @@
+﻿using AssetManagement.Application.Helpers;
+using AssetManagement.Domain.Entities;
+using AssetManagement.MVC.Models;
+using AutoMapper;
+
+namespace AssetManagement.MVC.Helpers;
+
+public class MvcMappingProfiles : Profile
+{
+    public MvcMappingProfiles()
+    {
+        CreateMap<BuildingViewModel, Building>()
+            .ForMember(dest => dest.Code, act => act.MapFrom(src => src.Code))
+            .ForMember(dest => dest.City, act => act.MapFrom(src => src.City))
+            .ForMember(dest => dest.PostCode, act => act.MapFrom(src => src.PostCode))
+            .ForMember(dest => dest.Street, act => act.MapFrom(src => src.Street));
+        CreateMap<Building, BuildingViewModel>()
+            .ForMember(dest => dest.Id, act => act.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Code, act => act.MapFrom(src => src.Code))
+            .ForMember(dest => dest.City, act => act.MapFrom(src => src.City))
+            .ForMember(dest => dest.PostCode, act => act.MapFrom(src => src.PostCode))
+            .ForMember(dest => dest.Street, act => act.MapFrom(src => src.Street));
+    }
+}
---
+++ AssetManagement.MVC/Models/BuildingViewModel.cs
@@ -0,0 +1,15 @@
+﻿using AssetManagement.Domain.Common;
+using AssetManagement.Domain.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AssetManagement.MVC.Models;
+
+public class BuildingViewModel
+{
+    public int? Id { get; set; }
+    public string? Code { get; set; } = null!;
+    public string? City { get; set; }
+    public string? PostCode { get; set; }
+    public string? Street { get; set; }
+    public List<string> SelectedFaculties { get; set; } = new List<string>();
+}

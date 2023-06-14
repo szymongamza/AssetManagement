0a9db58eee49c20e63c1a93ee565968079fb753b
Szymon Gamża <gamza.szymon@gmail.com>
2023-06-14 22:42:51 +0200

    Added service responses

---
+++ AssetManagement.Domain/Common/Responses/FacultyResponse.cs
@@ -0,0 +1,14 @@
+﻿using AssetManagement.Domain.Entities;
+
+namespace AssetManagement.Domain.Common.Responses;
+
+public class FacultyResponse : BaseResponse<Faculty>
+{
+    public FacultyResponse(Faculty resource) : base(resource)
+    {
+    }
+
+    public FacultyResponse(string message) : base(message)
+    {
+    }
+}
---
+++ AssetManagement.Domain/Common/Responses/RoomResponse.cs
@@ -0,0 +1,13 @@
+﻿using AssetManagement.Domain.Entities;
+
+namespace AssetManagement.Domain.Common.Responses; 
+public class RoomResponse : BaseResponse<Room>
+{
+    public RoomResponse(Room resource) : base(resource)
+    {
+    }
+
+    public RoomResponse(string message) : base(message)
+    {
+    }
+}

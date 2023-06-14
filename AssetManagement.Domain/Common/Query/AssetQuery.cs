e38faf6c3014c67cc5fbddec0ecd67cbba8eb8de
Szymon Gamża <gamza.szymon@gmail.com>
2023-06-14 22:42:28 +0200

    Asset query

---
+++ AssetManagement.Domain/Common/Query/AssetQuery.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace AssetManagement.Domain.Common.Query;
+public class AssetQuery : Query
+{
+    public AssetQuery(int page, int itemsPerPage) : base(page, itemsPerPage)
+    {
+    }
+}
